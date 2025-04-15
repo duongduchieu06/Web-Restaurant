5d62dd9871e777ea8c2a7705fcaed8d15e79a5ff
Hieu Duong Mabe <duonghieuyp06@gmail.com>
2025-04-15 15:19:52 +0700

    GOOD

--- backend/src/index.js
+++ backend/src/index.js
@@ -3,12 +3,13 @@ const dotenv = require("dotenv");
 const mongoose = require("mongoose");
 const routes = require('./routes/index');
 const cors = require('cors')
-const bodyParser = require("body-parser");
+// const bodyParser = require("body-parser");
 const cookieParser = require('cookie-parser');
+
 dotenv.config();
 
 const app = express();
-const port = process.env.PORT || 3001;
+// const port = process.env.PORT || 3001;
 
 app.use(cors())
 app.use(express.json({limit: '50mb'}))
@@ -18,14 +19,35 @@ app.use(cookieParser())
 routes(app);
 
 mongoose
-  .connect(`${process.env.MONGO_DB}`)
+  .connect(process.env.MONGO_DB, {
+    useNewUrlParser: true,
+    useUnifiedTopology: true,
+  })
   .then(() => {
-    console.log("connect DB is success!");
+    console.log("Connected to MongoDB!");
   })
   .catch((err) => {
-    console.log(err);
+    console.error("MongoDB connection error:", err);
   });
 
-app.listen(port, () => {
-  console.log("server is ng in port: ", port);
-});
+  if (process.env.NODE_ENV !== "production") {
+    const port = process.env.PORT || 3001;
+    app.listen(port, () => {
+      console.log("Server is running on port:", port);
+    });
+  }
+
+// mongoose
+//   .connect(`${process.env.MONGO_DB}`)
+//   .then(() => {
+//     console.log("connect DB is success!");
+//   })
+//   .catch((err) => {
+//     console.log(err);
+//   });
+
+// app.listen(port, () => {
+//   console.log("server is ng in port: ", port);
+// });
+
+module.exports = app;
