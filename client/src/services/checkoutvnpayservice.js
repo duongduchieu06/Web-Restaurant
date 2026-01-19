2ec0f70f91fd38f1f01e6b1977bbd63bac495dce
Hieu Duong Mabe <duonghieuyp06@gmail.com>
2026-01-19 21:01:28 +0700

    new

--- client/src/services/checkoutvnpayservice.js
+++ client/src/services/checkoutvnpayservice.js
@@ -2,11 +2,11 @@ import axios from 'axios';
 import * as request from '../utils';
 import { isJsonString } from '../utils';
 
+const apiBase = process.env.REACT_APP_API_URL || "http://localhost:3001/api";
+
 export const checkoutWithVNPAY = async (data) => {
   try {
-    // console.log("hieu")
-    const res = await axios.post("http://localhost:3001/api/checkout/create_payment_url", data)
-    // const res = await axios.post("https://chopsvn.vercel.app/api/checkout/create_payment_url", data)
+        const res = await axios.post(`${apiBase}/checkout/create_payment_url`, data)
     return res.data;
   } catch (error) {
     console.log("lỗi ở checkoutvnpayservice", error)
